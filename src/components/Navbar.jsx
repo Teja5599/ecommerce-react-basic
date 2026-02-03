1e154018cf6a0f6cea42982a5f7abf7578fbfc55
Teja5599 <satyateja88888@gmail.com>
2026-02-03 22:17:46 +0530

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
             </div>
             <div className='navbar-auth'>
                 <div className="navbar-auth-links">
-                    <Link to={'/auth '} className='btn btn-secondary'>Login</Link>
+                    <Link to={'/auth'} className='btn btn-secondary'>Login</Link>
                     <Link to={'/auth'} className='btn btn-primary'>SignUp</Link>
                 </div>
             </div>
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -7,7 +7,7 @@ const ProductCard = ({product}) => {
                 <img src={product.image} className='product-card-image' alt="" />
                 <div className='product-card-content'>
                     <h3 className='product-card-name'>{product.name}</h3>
-                    <p className='product-card-price'>{product.price}</p>
+                    <p className='product-card-price'>${product.price}</p>
                     <div className='product-card-actions'>
                         <Link className="btn btn-secondary">View Details</Link>
                         <button className='btn btn-primary'>Add to Cart</button>
