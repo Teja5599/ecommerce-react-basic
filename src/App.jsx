504da06c55495d59d55058ebad053ca78184b4be
Teja5599 <satyateja88888@gmail.com>
2026-02-05 18:03:19 +0530

    Added Auth

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import Home from './pages/Home'
 import Auth from './pages/Auth'
 import Checkout from './pages/Checkout'
 import Navbar from './components/Navbar'
+import AuthProvider from './context/AuthContext'
 
 function App() {
 
 
   return (
+    <AuthProvider>
     <div className='app'>
       <Navbar/>
       <Routes>
@@ -19,6 +21,7 @@ function App() {
       </Routes>
 
     </div>
+    </AuthProvider>
   )
 }
 
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,8 +1,86 @@
-import React from 'react'
-
+import React, { useContext, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { AuthContext } from '../context/AuthContext'
+import { useNavigate } from 'react-router-dom'
 const Auth = () => {
+
+    const[mode , setMode] = useState("signup")
+    const{register , formState : {errors} , handleSubmit} = useForm()
+    const{signUp , user , logout , login} = useContext(AuthContext)
+    const[error , setError] = useState(null)
+    const navigate = useNavigate()
+
+    function onSubmit(data){
+        setError(null)
+        let res;
+        if(mode === "signup"){
+            res = signUp(data.email , data.password);
+
+        }
+        else{
+            res = login(data.email , data.password);
+        }
+        if(res.success){
+            navigate("/")
+        }
+        else{
+            setError(res.error);
+        }
+        
+    }
   return (
-    <div>Auth</div>
+    <div className='page'>
+        <div className='container'>
+            <div className="auth-container">
+
+                {user && <p>User logged In : {user.email}</p>}
+                <button className='btn' onClick={()=> logout()}>Logout</button>
+                <h1 className='page-title'>
+                    {mode === "signup" ? 'Sign Up' : "Login"}
+                </h1>
+
+                <form className='auth-form' onSubmit={handleSubmit(onSubmit)}>
+                    {error && <div className='error-message'>{error}</div>}
+                    <div className='form-group'>
+                        <label className='form-label' htmlFor='email'>Email</label>
+                        <input type="email" className='form-input' id='email'
+                            {...register("email" , {required : "Email is required"})}
+                        />
+                        {errors.email && <span className='form-error'>{errors.email.message}</span>}
+                    </div>
+                    <div className='form-group'>
+                        <label className='form-label' htmlFor='password'>Password</label>
+                        <input
+                            {...register("password" , {required : "Password is required" ,
+                                minLength : {
+                                    value : 6,
+                                    message : 'Password must have 6 characters'
+                                } , 
+                                maxLength : {
+                                    value : 12,
+                                    message : 'Password must less than 12 characters'
+                                }
+                            })}
+                            type="password" className='form-input' id='password'
+                        />
+                        {errors.password && <span className='form-error'>{errors.password.message}</span>}
+                    </div>
+
+                    <button type='submit' className='btn btn-primary btn-large'>{mode === "signup" ? 'Sign Up' : "Login"}</button>
+                </form>
+
+                <div className="auth-switch">
+                    {mode === "signup" ? (
+                        <p>Already hava an account? <span onClick={()=>setMode("login")} className='auth-link'>Login</span></p>
+                         
+                    ) : (
+                        <p>Don't hava an account? <span onClick={()=>setMode("signup")} className='auth-link'>Sign Up</span></p>
+                    )}
+                   
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
