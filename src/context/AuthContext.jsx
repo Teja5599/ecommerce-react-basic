1115746912e2a5e5d68c0a31016f61412132cb1b
Teja5599 <satyateja88888@gmail.com>
2026-02-05 19:03:52 +0530

    Added Product details

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 export const AuthContext = createContext(null)
 
@@ -40,5 +40,11 @@ export default function AuthProvider({children}){
         localStorage.removeItem("currentuser")
         setUser(null)
     }
+
     return <AuthContext.Provider value={{signUp , user , logout , login}}>{children}</AuthContext.Provider>
+}
+
+export function useAuth(){
+    const context = useContext(AuthContext)
+    return context
 }
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,12 +1,12 @@
-import React, { useContext, useState } from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
-import { AuthContext } from '../context/AuthContext'
+import { useAuth } from '../context/AuthContext'
 import { useNavigate } from 'react-router-dom'
 const Auth = () => {
 
     const[mode , setMode] = useState("signup")
     const{register , formState : {errors} , handleSubmit} = useForm()
-    const{signUp , user , logout , login} = useContext(AuthContext)
+    const{signUp , login} = useAuth()
     const[error , setError] = useState(null)
     const navigate = useNavigate()
 
@@ -33,8 +33,8 @@ const Auth = () => {
         <div className='container'>
             <div className="auth-container">
 
-                {user && <p>User logged In : {user.email}</p>}
-                <button className='btn' onClick={()=> logout()}>Logout</button>
+                
+              
                 <h1 className='page-title'>
                     {mode === "signup" ? 'Sign Up' : "Login"}
                 </h1>
@@ -83,5 +83,7 @@ const Auth = () => {
     </div>
   )
 }
+
+
 
 export default Auth
